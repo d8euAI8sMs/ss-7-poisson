d1654c0491586315826121222e8b3ebf860e5d97
kalaider <a.kalaider@yandex.ru>
2017-12-13 22:04:17 +0300

    [c] System parameters, materials and related code added

--- src/poisson/model.h
+++ src/poisson/model.h
@@ -13,14 +13,40 @@ namespace model
 
     struct parameters
     {
+        // system params
+        double a, b, k;
+
+        // capacitor params
+        double d;
+
+        // Faraday cage params
+        double d_c, x_c, y_c, a_c, b_c;
+
         // other params
         double dt, dy, dx;
+
+        // material params
+        double eps, q1, q2;
     };
 
     inline static parameters make_default_parameters()
     {
         return
         {
+            // system params
+            50, 50, 2,
+
+            // capacitor params
+            5,
+
+            // Faraday cage params
+            5, 0, 0, 20, 20,
+
+            // other params
+            0.3, 1, 1,
+
+            // material params
+            1, 1, 1
         };
     }
 
@@ -50,6 +76,8 @@ namespace model
     inline static void adjust(parameters & params,
                               plot_data  & data)
     {
+        data.world->xmin = - (data.world->xmax = params.a * params.k);
+        data.world->xmin = - (data.world->ymax = params.b * params.k);
     }
 
     inline static plot::drawable::ptr_t make_root_drawable
@@ -117,6 +145,10 @@ namespace model
         const material_t border_i = 0x1 << 0;
         const material_t border_j = 0x1 << 1;
         const material_t ext      = 0x1 << 4;
+        const material_t metal    = 0x1 << 5;
+        const material_t dielectr = 0x1 << 6;
+        const material_t cap1     = 0x1 << 7;
+        const material_t cap2     = 0x1 << 8;
 
         const material_t border   = border_i | border_j;
         const material_t flags    = border;
@@ -166,6 +198,152 @@ namespace model
 
     inline void make_chasing_data(chasing_data & d, const parameters & p)
     {
+        d.n = (size_t) std::ceil((p.b * p.k) / p.dy) * 2;
+        d.m = (size_t) std::ceil((p.a * p.k) / p.dx) * 2;
+
+        d.area_map.clear();
+        d.fn.clear();
+
+        d.area_map.resize(d.n, std::vector < material_t > (d.m));
+        d.fn.resize(d.n, std::vector < double > (d.m));
+
+        size_t Y_n  = (size_t) std::ceil((p.b * p.k) / p.dy);
+        size_t X_m  = (size_t) std::ceil((p.a * p.k) / p.dx);
+        size_t yc_n = (size_t) std::ceil(p.y_c / p.dy);
+        size_t xc_m = (size_t) std::ceil(p.x_c / p.dx);
+        size_t b_n  = (size_t) std::ceil(p.b / p.dy);
+        size_t a_m  = (size_t) std::ceil(p.a / p.dx);
+        size_t bc_n  = (size_t) std::ceil(p.b_c / p.dy);
+        size_t ac_m  = (size_t) std::ceil(p.a_c / p.dx);
+        size_t d_n  = (size_t) std::ceil(p.d / p.dy);
+        size_t d_m  = (size_t) std::ceil(p.d / p.dx);
+        size_t dc_n = (size_t) std::ceil(p.d_c / p.dy);
+        size_t dc_m = (size_t) std::ceil(p.d_c / p.dx);
+
+        // set up materials and sketch out borders
+
+        for (size_t i = 0; i < d.n; ++i)
+        {
+            for (size_t j = 0; j < d.m; ++j)
+            {
+                bool is_border           = (i == 0) || ((i + 1) == Y_n * 2) || (j == 0) || ((j + 1) == X_m * 2);
+                bool is_capacitor        = is_in_rect({ i, j }, { Y_n - b_n, Y_n + b_n, X_m - a_m, X_m - a_m + d_m })
+                                        || is_in_rect({ i, j }, { Y_n - b_n, Y_n + b_n, X_m + a_m - d_m, X_m + a_m });
+                bool is_capacitor_border = is_capacitor && (
+                                                !is_in_rect({ i, j }, { Y_n - b_n + 1, Y_n + b_n - 1, X_m - a_m + 1, X_m - a_m + d_m - 1 })
+                                             && !is_in_rect({ i, j }, { Y_n - b_n + 1, Y_n + b_n - 1, X_m + a_m - d_m + 1, X_m + a_m - 1 })
+                                         );
+                bool is_cage_inner       = is_in_rect({ i, j }, { Y_n - yc_n - bc_n + dc_n, Y_n - yc_n + bc_n - dc_n, X_m + xc_m - ac_m + dc_m, X_m + xc_m + ac_m - dc_m });
+                bool is_cage             = !is_cage_inner && is_in_rect({ i, j }, { Y_n - yc_n - bc_n, Y_n - yc_n + bc_n, X_m + xc_m - ac_m, X_m + xc_m + ac_m });
+                bool is_cage_border      = is_cage && !is_in_rect({ i, j }, { Y_n - yc_n - bc_n + 1, Y_n - yc_n + bc_n - 1, X_m + xc_m - ac_m + 1, X_m + xc_m + ac_m - 1 })
+                                        || !is_cage_inner && is_in_rect({ i, j }, { Y_n - yc_n - bc_n + dc_n - 1, Y_n - yc_n + bc_n - dc_n + 1, X_m + xc_m - ac_m + dc_m - 1, X_m + xc_m + ac_m - dc_m + 1 });
+
+                if (is_border || is_capacitor_border || is_cage_border)
+                    d.area_map[i][j]  = material::border;
+                if (is_capacitor || is_cage || is_border)
+                    d.area_map[i][j] |= material::metal;
+                else
+                    d.area_map[i][j]  = material::dielectr;
+
+                if (is_capacitor && (j < Y_n)) d.fn[i][j] = p.q1;
+                if (is_capacitor && (j > Y_n)) d.fn[i][j] = p.q2;
+            }
+        }
+
+        // detect border orientations
+
+        for (size_t i = 0; i < d.n; ++i)
+        {
+            for (size_t j = 0; j < d.m; ++j)
+            {
+                if (get_material_at(d, { (int) i, (int) j }) & material::border)
+                {
+                    d.area_map[i][j] &= ~material::border;
+                    if ((get_material_at(d, { (int) i - 1, (int) j })
+                        | get_material_at(d, { (int) i + 1, (int) j })) & material::border)
+                    {
+                        d.area_map[i][j] |= material::border_j;
+                    }
+                    if ((get_material_at(d, { (int) i, (int) j - 1 })
+                        | get_material_at(d, { (int) i, (int) j + 1 })) & material::border)
+                    {
+                        d.area_map[i][j] |= material::border_i;
+                    }
+                }
+            }
+        }
+
+        d.coefs = [&] (size_t i, size_t j, chasing_dir dir,
+                       const chasing_data & d,
+                       const parameters & p,
+                       std::vector < std::vector < double > > & T) -> chasing_coefs
+        {
+            // make ordinary A B C D coefficients
+            auto make_normal_coefs = [&] (
+                size_t i, size_t j, chasing_dir s,
+                double eps) -> chasing_coefs
+            {
+                bool is_ext_i = (get_material_at(d, { (int) i - 1, ( int) j })
+                                 | get_material_at(d, { (int) i + 1, ( int) j })) & material::ext;
+                bool is_ext_j = (get_material_at(d, { (int) i, ( int) j - 1 })
+                                 | get_material_at(d, { (int) i, ( int) j + 1 })) & material::ext;
+
+                if (s == chasing_dir::i)
+                {
+                    return
+                    {
+                        - p.dt / 2. / p.dy / p.dy,
+                        - p.dt / 2. / p.dy / p.dy,
+                        1 + p.dt / 1. / p.dy / p.dy,
+                        T[i][j] + p.dt / 2. *
+                        (
+                            (is_ext_j ? 0 : ((T[i][j + 1] + T[i][j - 1] - 2 * T[i][j]) / p.dx / p.dx))
+                        ) + p.dt / 2. * d.fn[i][j] / eps
+                    };
+                }
+                return
+                {
+                    - p.dt / 2. / p.dx / p.dx,
+                    - p.dt / 2. / p.dx / p.dx,
+                    1 + p.dt / 1. / p.dx / p.dx,
+                    T[i][j] + p.dt / 2. *
+                    (
+                        (is_ext_i ? 0 : ((T[i + 1][j] + T[i - 1][j] - 2 * T[i][j]) / p.dy / p.dy))
+                    ) + p.dt / 2. * d.fn[i][j] / eps
+                };
+            };
+
+            material_t m = d.area_map[i][j];
+
+            // deal with boundary conditions
+            if (m & material::border)
+            {
+                if (m & material::metal)
+                {
+                    if (m & material::cap1)
+                    {
+                        return { 0, 0, 1, p.q1 };
+                    }
+                    else if (m & material::cap2)
+                    {
+                        return { 0, 0, 1, p.q2 };
+                    }
+                    else
+                    {
+                        return { 0, 0, 1, 0 };
+                    }
+                }
+            }
+            // deal with ordinary space
+            else if (!(m & material::metal))
+            {
+                return make_normal_coefs(i, j, dir, p.eps);
+            }
+            else
+            {
+                return { 0, 0, 1, 0 };
+            }
+        };
     }
 
     // post-apply boundary conditions again
@@ -305,7 +483,7 @@ namespace model
 
     inline static stencil_fn make_material_based_stencil(const chasing_data & d)
     {
-        return [&] (int i, int j) { return !(get_material_at(d, { i, j }) & material::ext); };
+        return [&] (int i, int j) { return !(get_material_at(d, { i, j }) & (material::ext | material::metal)); };
     }
 
     void find_isolines
@@ -329,6 +507,43 @@ namespace model
         return [&] (CDC & dc, const plot::viewport & vp)
         {
             if (d.area_map.empty()) return;
+
+            auto metal_brush  = plot::palette::brush(RGB(100, 100, 100));
+            auto border_brush = plot::palette::brush(RGB( 50,  50,  50));
+
+            RECT r;
+
+            material_t m;
+
+            for (size_t i = 0; i < d.n; ++i)
+            {
+                for (size_t j = 0; j < d.m; ++j)
+                {
+                    m = get_material_at(d, { (int) i, (int) j });
+
+                    if (m & material::ext) continue;
+
+                    if (m & (material::border | material::metal))
+                    {
+                        r = xyxy(vp,
+                        {
+                            ((double)j - 0.5) * params.dx,
+                            ((double)j + 0.5) * params.dx,
+                            ((double)i - 0.5) * params.dy,
+                            ((double)i + 0.5) * params.dy
+                        });
+
+                        if (m & material::border)
+                        {
+                            dc.FillRect(&r, border_brush.get());
+                        }
+                        else if (m & material::metal)
+                        {
+                            dc.FillRect(&r, metal_brush.get());
+                        }
+                    }
+                }
+            }
         };
     }
 }
