23c403fe880d1f72603fe9db33d1ffa6ad2ef897
kalaider <a.kalaider@yandex.ru>
2017-12-21 18:11:31 +0300

    [c] Color scheme changed

--- src/poisson/model.h
+++ src/poisson/model.h
@@ -317,8 +317,8 @@ namespace model
         {
             if (d.area_map.empty()) return;
 
-            auto metal_brush  = plot::palette::brush(RGB(200,   0,   0));
-            auto border_brush = plot::palette::brush(RGB(255, 255,   0));
+            auto metal_brush  = plot::palette::brush(RGB(100, 100, 100));
+            auto border_brush = plot::palette::brush(RGB(155, 155, 155));
 
             RECT r;
 
